d59e0ea9062b812c7746f59a90f7c9823e5131b0
Kudo Chien <kudo@expo.dev>
2021-11-23 10:46:40 +0800

    add comments

--- packages/expo-modules-core/ios/ReactDelegates/ModulePriorities.swift
+++ packages/expo-modules-core/ios/ReactDelegates/ModulePriorities.swift
@@ -1,7 +1,15 @@
 // Copyright 2018-present 650 Industries. All rights reserved.
 
+/**
+ This class determines the order of `EXReactDelegateHandler`.
+
+ The priority is only for internal use and we maintain a pre-defined `SUPPORTED_MODULE` map.
+ */
 internal struct ModulePriorities {
   static let SUPPORTED_MODULE = [
+    // {key}: {value}
+    // key: node package name
+    // value: priority value, the higher value takes precedence
     "expo-screen-orientation": 10,
     "expo-updates": 5,
   ]
